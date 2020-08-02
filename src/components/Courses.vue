686b5e39ad4970aa643f31de6726ac9b7d059101
Robin Hamill <hi@robinhamil.com>
2020-08-02 13:15:42 -0400

    Update templates

---
+++ src/components/Courses.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <h2>Courses</h2>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'Courses',
+  components: {
+  },
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<style scoped lang="stylus">
+@import '../styles/_vars'
+
+
+</style>
